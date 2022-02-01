a9395af4f29971885257bf9ab4350acd513860e6
mschoenebeck <matthias.schoenebeck@gmail.com>
2022-02-01 02:06:13 -0500

    updated Makefile

--- zeos.cpp
+++ zeos.cpp
@@ -3,8 +3,7 @@
 #include <string>
 #include <vector>
 
-#include "json_struct.h"
-#include "base64.h"
+#include <json_struct.h>
 #include "groth16/verifier.hpp"
 
 #include <emscripten.h>
@@ -12,13 +11,8 @@
 extern "C" {
 
 EMSCRIPTEN_KEEPALIVE
-//bool verify_proof(char* vk_b46, char* proof_b46, char* inputs_b46)
 bool verify_proof(char* vk_str, char* proof_str, char* inputs_str)
 {
-    //std::string vk_str = base64_decode(vk_b46);
-    //std::string proof_str = base64_decode(proof_b46);
-    //std::string inputs_str = base64_decode(inputs_b46);
-    
     Proof proof;
     VerifyingKey vk;
     PreparedVerifyingKey pvk;
