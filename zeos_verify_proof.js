e27ee597c395f79c2a9ed000c4db311216f70de3
mschoenebeck <matthias.schoenebeck@gmail.com>
2022-07-18 21:07:56 -0500

    zeos_verify_proof handler works now (tested with contract and groth16 proof example)

--- zeos_verify_proof.js
+++ zeos_verify_proof.js
@@ -1,13 +1,23 @@
-var zeos_verifier = require('./pkg/zeos_verifier.js');
-var zeos_groth16 = require('./cpp-verifier/zeos_groth16.js');
+const zeos = import('./pkg/zeos_verifier.js');
+
 const fetch = require("node-fetch");
 global.fetch = fetch;
 
 // TODO: might require: npm i --save @liquidapps/dapp-client.
 const { createClient } = require('@liquidapps/dapp-client');
 
+// Convert a hex string to a byte array
+function hex2Bytes(hex)
+{
+  for(var bytes = [], c = 0; c < hex.length; c += 2)
+      bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
 module.exports = async ({ proto, address }) => {
-  // zeos_verify_groth16://  
+  // Format:
+  // zeos_verify_proof://halo2|groth16/thezeostoken/testkeyzeos1/[..]/[..]
+  //        proto       |             address
   
   // split address to extract parameters
   const payloadParts = address.split('/');
@@ -18,25 +28,30 @@ module.exports = async ({ proto, address }) => {
   var proof_str = payloadParts[idx++];
   var inputs_str = payloadParts[idx++];
   
-  ////////////////////////////////////////////////////////////////////////////////////
-  // TODO: update file from here downwards according to new 'zeos_verifier' library //
-  ////////////////////////////////////////////////////////////////////////////////////
-  
   // fetch verifier key from vram
   // TODO: for zeus use port 13015, for live environment on DSPs use port 3115
   let dappClient = await createClient({ httpEndpoint: "http://localhost:13015", fetch });
   let vramClient = await dappClient.service("ipfs", "thezeostoken");
-  var response = await vramClient.get_vram_row("thezeostoken", vk_code, "verifierkey", vk_id);
+  var response = await vramClient.get_vram_row("thezeostoken", vk_code, "vk", vk_id);
   var vk_str = response.row.vk;
   
-  var res;
-  await zeos_groth16().then((instance) => {
-    var vk_ptr = instance.allocate(instance.intArrayFromString(vk_str), instance.ALLOC_NORMAL);
-    var proof_ptr = instance.allocate(instance.intArrayFromString(proof_str), instance.ALLOC_NORMAL);
-    var inputs_ptr = instance.allocate(instance.intArrayFromString(inputs_str), instance.ALLOC_NORMAL);
-  
-    res = instance._verify_proof(vk_ptr, proof_ptr, inputs_ptr);
-  }); 
+  var res = false;
+  await zeos.then(m => {
+    switch(type)
+    {
+      case "halo2":
+        res = m.verify_halo2_proof(hex2Bytes(proof_str), hex2Bytes(inputs_str), hex2Bytes(vk_str));
+        break;
+
+      case "groth16":
+        res = m.verify_groth16_proof(hex2Bytes(proof_str), hex2Bytes(inputs_str), hex2Bytes(vk_str));
+        break;
+
+      default:
+        res = false;
+        break;
+    }
+  }).catch(console.error);
 
   if(res)
   {
